ccd48c034b86eccf15a2a25de72b31bb1c4520fe
EBroestl <broestl.e@gmail.com>
2021-06-21 18:12:22 -0500

    initial 2

--- Q9_EBroestl.sql
+++ Q9_EBroestl.sql
@@ -1,41 +1,5 @@
 /*9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
-Give their full name and the teams that they were managing when they won the award.
-
-WITH award_winners AS (select aw1.playerid AS p_id,
-	   aw1.awardid AS aw1,
-	   aw2.awardid AS aw2,
-	   aw1.lgid AS lgid1,
-	   aw1.yearid AS y1,			   
-	   aw2.lgid AS lgid2,
-	   aw2.yearid AS y2,
-	   aw1.playerid AS pid
-from awardsmanagers AS aw1 INNER JOIN awardsmanagers AS aw2
-USING (playerid)
-WHERE aw2.awardid ILIKE'%tsn%'
-AND aw1.awardid ILIKE'%tsn%'
-AND aw1.lgid ='AL'
-AND aw2.lgid ='NL')
-
-SELECT  
-		aw.pid,
-		aw.aw1,
-		p.namefirst,
-	    p.namelast,
-	    t.name,
-	    aw.aw1,
-		aw.lgid1,
-		aw.lgid2,
-		aw.y1,
-		aw.y2,
-		t.yearid
-		
-from award_winners AS aw 
-INNER JOIN people AS p ON aw.p_id=p.playerid
-INNER JOIN appearances AS a on p.playerid=a.playerid
-INNER JOIN teams AS t on a.teamid=t.teamid
-WHERE aw.pid = a.playerid
-and aw.y1=t.yearid
-and aw.pid=p.playerid;*/
+Give their full name and the teams that they were managing when they won the award. */
 
 
 WITH aw AS (SELECT DISTINCT
